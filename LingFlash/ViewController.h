1f2c6387e5eceaae2819e736f4ff6726a28a8af4
Thomas Dattilo <tom@dattilo.io>
2019-02-21 00:42:11 +0100

    Initial Commit

---
+++ LingFlash/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LingFlash
+//
+//  Created by Thomas Dattilo on 2/21/19.
+//  Copyright © 2019 Thomas Dattilo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LingFlash/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LingFlash
+//
+//  Created by Thomas Dattilo on 2/21/19.
+//  Copyright © 2019 Thomas Dattilo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
